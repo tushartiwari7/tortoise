038d13b8082cf29fa91fdf423f0fe04bfffe087f
tushartiwari7 <tushartiwari0172@gmail.com>
2022-07-26 20:25:01 +0530

    [feat]: playing game page

--- src/pages/PlaygamePage/PlaygamePage.jsx
+++ src/pages/PlaygamePage/PlaygamePage.jsx
@@ -1,6 +1,39 @@
+import "./PlaygamePage.css";
+import { Layout, Card, Typography, Input, Button, Row, Col } from "antd";
 
 const PlaygamePage = () => {
-  return <h1>Play game</h1>;
+  return (
+    <Layout className="full-width">
+      <Layout.Header className="game-header">
+        <Row>
+          <Col className="game-heading">
+            <Typography.Title type="secondary" level={2}>
+              Type the Text
+            </Typography.Title>
+          </Col>
+          <Col className="timer-container">
+            <Typography.Title level={2}>10:00</Typography.Title>
+          </Col>
+        </Row>
+      </Layout.Header>
+
+      <Layout.Content>
+        <Card className="alphabet-display-card">
+          <Typography.Title level={1}>A</Typography.Title>
+        </Card>
+      </Layout.Content>
+      <Layout.Footer>
+        <Row>
+          <Col className="input-box">
+            <Input placeholder="Type Alphabet"></Input>
+          </Col>
+          <Col>
+            <Button>Reset</Button>
+          </Col>
+        </Row>
+      </Layout.Footer>
+    </Layout>
+  );
 };
 
 export default PlaygamePage;
